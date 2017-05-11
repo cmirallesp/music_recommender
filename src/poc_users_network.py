ea24b639c94041c4b51d5c980da727ed41ca1894
Carles <cmirallesp@gmail.com>
2017-05-11 11:02:23 +0200

    Add networkx

--- src/poc_users_network.py
+++ src/poc_users_network.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import graph_tool.all as gt
 import igraph as ig
+import networkx as nx
 import time
 import pdb
 # user_artists = pd.read_table('user_artists.dat')
@@ -58,6 +59,33 @@ def graph_igraph(user_friends):
         _graph.add_edge(v1, v2)
     return _graph
 
+
+def graph_nx(user_friends):
+    _graph = nx.Graph(directed=False)
+    # double|bool|object(dict or whatever)
+
+    added = {}
+    for uid, fid in user_friends.values:
+
+        if uid in added:
+            v1 = added[uid]
+        else:
+            _graph.add_node(uid, _id=uid, type="user")
+            v1 = _graph.node[uid]
+
+            added[uid] = v1
+
+        if fid in added:
+            v2 = added[fid]
+        else:
+            _graph.add_node(fid, _id=fid, type="user")
+            v2 = _graph.node[fid]
+
+            added[fid] = v2
+
+        _graph.add_edge(v1['_id'], v2['_id'])
+    return _graph
+
 user_friends = pd.read_table('../data/user_friends.dat')
 
 start = time.time()
@@ -69,11 +97,20 @@ for v in g.get_vertices()[:10]:
         pdb.set_trace()
     # access to properties
     print "{}-{}".format(g.vp.id[v], g.vp.type[v])
-print "time graphtool: {}".format(end - start)
+print "time graphtool: {}-{}".format(end - start, g.get_vertices().shape[0])
 
 start = time.time()
 g = graph_igraph(user_friends)
 end = time.time()
 for vtx in g.vs[:10]:
     print "{}-{}".format(vtx["id"], vtx["type"])
-print "time igraph: {}".format(end - start)
+print "time igraph: {}-{}".format(end - start, len(g.vs))
+
+start = time.time()
+g = graph_nx(user_friends)
+end = time.time()
+gg = list(g)
+for v in gg[:10]:
+    vtx = g.node[v]
+    print "{}-{}".format(vtx["_id"], vtx["type"])
+print "time networkx: {}-{}".format(end - start, len(gg))
