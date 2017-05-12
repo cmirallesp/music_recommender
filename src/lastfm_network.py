ca091a2144c52c8194824b7a820cffcb4a05dcae
Carles <cmirallesp@gmail.com>
2017-05-12 10:05:10 +0200

    Added lastfm_network class

---
+++ src/lastfm_network.py
@@ -0,0 +1,47 @@
+import networkx as nx
+
+
+class LastfmNetwork(object):
+    """docstring for LastfmNetwork"""
+
+    def __init__(self, user_friends, user_artists, user_taggedartists):
+        super(LastfmNetwork, self).__init__()
+        # multilayer graph to hold the entire data
+        self._graph = nx.Graph(directed=False)
+        self._build_user_friends(user_friends)
+
+    def key_user(self, id):
+        return "u_{}".format(id)
+
+    def key_artist(self, id):
+        return "l_{}".format(id)
+
+    def _build_user_friends(self, user_friends):
+        added = {}
+        for uid, fid in user_friends.values:
+            k = self.key_user(uid)
+            k2 = self.key_user(fid)
+
+            if k in added:
+                v1 = added[k]
+            else:
+                self._graph.add_node(k, type="user")
+                v1 = self._graph.node[k]
+
+                added[k] = v1
+
+            if k2 in added:
+                v2 = added[k2]
+            else:
+                self._graph.add_node(k2, type="user")
+                v2 = self._graph.node[k2]
+
+                added[k2] = v2
+
+            self._graph.add_edge(k, k2)
+
+    def friends(self, user_id1, user_id2):
+        return self._graph.has_edge(
+            self.key_user(user_id1),
+            self.key_user(user_id2)
+        )
---
+++ src/test_lastfm_network.py
@@ -0,0 +1,21 @@
+from lastfm_network import *
+import pandas as pd
+import unittest
+
+
+class TestLastfmNetwork(unittest.TestCase):
+
+    def setUp(self):
+        self.lastfm_net = LastfmNetwork(
+            user_friends=pd.read_table('../data/user_friends.dat'),
+            user_artists=pd.read_table('../data/user_artists.dat'),
+            user_taggedartists=pd.read_table('../data/user_friends.dat')
+        )
+
+    def test_friends(self):
+        self.assertTrue(self.lastfm_net.friends(2, 275))
+        self.assertFalse(self.lastfm_net.friends(2, 2))
+
+
+if __name__ == '__main__':
+    unittest.main()
