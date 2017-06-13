dd9e7f780905905ec4a69dc02cc84c45497b3653
Carles <cmirallesp@gmail.com>
2017-06-13 02:18:28 +0200

    Backup

--- src/descriptors.py
+++ src/descriptors.py
@@ -1,3 +1,4 @@
+
 from pylab import *
 from lastfm_network import LastfmNetwork
 import time
@@ -9,20 +10,21 @@ import graph_tool.topology as top
 import graph_tool.clustering as clu
 import os
 import networkx as nx
-import pdb
+
 import seaborn as sns  # pip install seaborn
+
 sns.set_palette("deep", desat=.6)
 sns.set_style("whitegrid")
 
 PLOT_TITLES = {
-    'sp': "Shortest Path distribution [{} > {}]",
-    'cd': "Components distribution [{} > {}]",
-    'wd': "Weight distribution [{} > {}]",
-    'at': "{} degree distribution [Artists > Tags]",
+    'sp': "Shortest Path distribution [{} - {}]",
+    'cd': "Components distribution [{} - {}]",
+    'wd': "{}Weight distribution [{} - {}]",
+    'at': "{}{} degree distribution [Artists - Tags]",
     'ta': "{} degree distribution [Tags > Artists]",
-    'au': "{} degree distribution [Artists > Users]",
+    'au': "{}{} degree distribution [Artists - Users]",
     'ua': "{} degree distribution [Users > Artists]",
-    'uu': "{} degree distribution [Users > Users]",
+    'uu': "{}{} degree distribution [Users - Users]",
 }
 
 
@@ -38,24 +40,113 @@ def gt_network(nx_network, file_name):
     return gt_net
 
 
-def draw_hist(x, y, title, file_name, loglog=False, bins=15):
-    y_ = y[y > 0]
-    N = y_.shape[0]
-    x1 = x.min()
-    x2 = x.max()
+def draw_hist(x, y, title, file_name, loglog=False, nbins=10, cumulative=False):
+    x1, x2 = min(x), max(x)
+
     if loglog:
-        # x_ = np.logspace(np.log10(1), np.log10(N), N)
-        x_ = np.logspace(np.log10(max(x1, 1)), np.log10(x2), N)
+        bins = np.logspace(np.log10(max(x1, 1)), np.log10(x2), nbins)
     else:
-        # x_ = np.linspace(1, N, N)
-        x_ = np.linspace(x1, x2, N)
+        bins = np.linspace(x1, x2, nbins)
 
     plt.figure()
+    ax = plt.gca()
+    ttl = ax.title
+    ttl.set_position([.5, 1.05])
     plt.title(title)
+    if loglog:
+        plt.yscale('log')
+        plt.xscale('log')
 
-    plt.hist(x_, weights=y_, log=loglog, bins=bins)
+    plt.xticks(bins, ["{}".format(i) for i in np.round((bins), 2)], fontsize=10, rotation=45)
+
+    bb, cc, patches = plt.hist(x, weights=y, log=loglog, bins=bins, cumulative=cumulative, normed=True)
+    for i, _ in enumerate(bb):
+        w = (cc[i + 1] - cc[i])
+
+        area = bb[i] if cumulative else bb[i] * w
+        patch = patches[i]
+        h = patch.get_height()
+        plt.text(patch.get_x() + patch.get_width() / 2, h, "{}".format(round(area, 3)), ha='center', va='bottom', fontsize=10, color='gray')
+
     # plt.plot(x_, y_)
     plt.savefig('plots/desc/{}.png'.format(file_name))
+    plt.close()
+
+
+def configureXAxis(kmin, kmax):
+    #maxOrderX = np.floor(kmax+1 - 10**math.log10(kmax+1))
+    if kmin == 0:
+        xmin = kmin
+    else:
+        minOrderX = 10**np.floor(math.log10(kmin))
+        xmin = np.ceil((kmin - minOrderX) / minOrderX) * minOrderX
+    maxOrderX = 10**np.floor(math.log10(kmax + 1))
+    xmax = np.ceil((kmax + 1) / maxOrderX) * maxOrderX
+    return xmin, xmax
+
+
+def configureYAxis(hist):
+    ymax = 1
+    minOrderOfMagnitude = math.log10(np.min([p for p in hist if p != 0]))
+    if abs(minOrderOfMagnitude - np.floor(minOrderOfMagnitude)) <= 0.00001:
+        ymin = 10**np.floor(minOrderOfMagnitude) - 1 * 10**(np.floor(minOrderOfMagnitude) - 1)
+    else:
+        ymin = 10**np.floor(minOrderOfMagnitude)
+    return ymin, ymax
+
+
+def plot_hist(x, title, file_name, loglog=False, nbins=15, cumulative=False):
+    x_ = np.array(x)
+    x1, x2 = x_.min(), x_.max()
+
+    if loglog:
+        bins = np.logspace(np.log10(max(1, x1)), np.log10(x2), nbins)
+    else:
+        # x_ = np.linspace(1, N, N)
+        bins = np.linspace(x1, x2, nbins)
+
+    plt.figure()
+    ax = plt.gca()
+    ttl = ax.title
+    ttl.set_position([.5, 1.05])
+    plt.title(title)
+
+    if loglog:
+        plt.yscale('log')
+        plt.xscale('log')
+
+    if loglog:
+        plt.xticks(bins, ["${}$".format(i) for i in np.round((bins), 2)], fontsize=10, rotation=45)
+    else:
+        plt.xticks(bins, ["{}".format(i) for i in np.round(bins, 1)], fontsize=10, rotation=45)
+
+    try:
+        bb, cc, patches = plt.hist(x, bins=bins, normed=True, cumulative=cumulative, log=loglog)
+        axes = plt.gca()
+
+        ymin, ymax = configureYAxis(bb)
+        axes.set_ylim([ymin, ymax])
+        xmin, xmax = configureXAxis(x1, x2)
+        axes.set_xlim([xmin, xmax])
+
+        # bb2, cc2 = np.histogram(x, bins=bins)
+
+        for i, _ in enumerate(bb):
+            w = (cc[i + 1] - cc[i])
+            area = bb[i] if cumulative else bb[i] * w
+
+            patch = patches[i]
+            h = patch.get_height()
+            plt.text(patch.get_x() + patch.get_width() / 2, h, "{}".format(round(area, 3)), ha='center', va='bottom', fontsize=10, color='gray')
+
+    except Exception:
+        print "Here"
+        plt.hist(x, log=loglog, bins=bins, normed=True, cumulative=cumulative)
+    finally:
+        # pdb.set_trace()
+        plt.savefig('plots/desc/{}.png'.format(file_name))
+        # pdb.set_trace()
+        plt.close()
 
 
 def shortest_paths(network):
@@ -69,11 +160,11 @@ def shortest_paths(network):
 
 def get_descriptors(network, short_name, nx_network, already_calculated=False):
     def _prefixToTitle(prefix):
-        if prefix == 'A':
+        if prefix == 'a':
             return "Artists"
-        elif prefix == 'T':
+        elif prefix == 't':
             return "Tags"
-        elif prefix == 'U':
+        elif prefix == 'u':
             return 'Users'
 
     filename = "cache/{}.pickle".format(short_name)
@@ -86,25 +177,34 @@ def get_descriptors(network, short_name, nx_network, already_calculated=False):
     t1 = _prefixToTitle(prefix1)
     t2 = _prefixToTitle(prefix2)
     result['name'] = short_name
-    result['title_dd1'] = PLOT_TITLES[short_name].format(t1)
-    result['title_dd2'] = PLOT_TITLES[short_name].format(t2)
-    result['title_wd'] = PLOT_TITLES['wd'].format(t1, t2)
+    result['title_dd1'] = PLOT_TITLES[short_name].format(t1, "")
+    result['title_dd2'] = PLOT_TITLES[short_name].format(t2, "")
+    result['title_dd1_acum'] = PLOT_TITLES[short_name].format(t1, " Cumulative")
+    result['title_dd2_acum'] = PLOT_TITLES[short_name].format(t2, " Cumulative")
+    result['title_wd'] = PLOT_TITLES['wd'].format("", t1, t2)
+    result['title_wd_acum'] = PLOT_TITLES['wd'].format("Cumulative ", t1, t2)
     result['title_cd'] = PLOT_TITLES['cd'].format(t1, t2)
     result['title_sp'] = PLOT_TITLES['sp'].format(t1, t2)
     result['filename_dd'] = '{}_dd'.format(short_name)  # degree input dist
     result['filename_ddl'] = '{}_dd_log'.format(short_name)  # degree dist (log)
     result['filename_dd1'] = '{}_{}_dd'.format(short_name[0], short_name)  # degree input dist
     result['filename_dd2'] = '{}_{}_dd'.format(short_name[1], short_name)  # degree input dist
+    result['filename_dd1l'] = '{}_{}_dd_log'.format(short_name[0], short_name)  # degree input dist
+    result['filename_dd2l'] = '{}_{}_dd_log'.format(short_name[1], short_name)  # degree input dist
+    result['filename_dd1_acum'] = '{}_{}_dd_acum'.format(short_name[0], short_name)  # degree input dist
+    result['filename_dd2_acum'] = '{}_{}_dd_acum'.format(short_name[1], short_name)  # degree input dist
     result['filename_wd'] = '{}_wd'.format(short_name)  # weight distribution
     result['filename_wdl'] = '{}_wd_log'.format(short_name)  # weight distribution
+    result['filename_wd_acum'] = '{}_wd_acum'.format(short_name)  # weight distribution
     result['filename_sp'] = '{}_sp'.format(short_name)  # shortest path
     result['filename_cd'] = '{}_cd'.format(short_name)  # components
     result['filename_cdl'] = '{}_cd_log'.format(short_name)  #
 
     nodes = network.get_vertices()
     edges = network.get_edges()
+    result['num_nodes'] = {}
+    result['num_nodes']['total'] = nodes.shape[0]
 
-    result['num_nodes'] = nodes.shape[0]
     result['num_edges'] = edges.shape[0]
 
     result['degree'] = {"total": {}, "prefix1": {}, "prefix2": {}}
@@ -113,23 +213,27 @@ def get_descriptors(network, short_name, nx_network, already_calculated=False):
     result['degree']["total"]['avg'] = network.get_out_degrees(nodes).mean()
     result['degree']["total"]["counts"], result['degree']["total"]["bins"] = st.vertex_hist(network, "out")
 
-    lst1, lst2 = [], []
+    nodes1, nodes2 = [], []
     for node in nodes:
         if prefix1 in network.vp['id'][node]:
-            lst1.append(node)
+            nodes1.append(node)
         elif prefix2 in network.vp['id'][node]:
-            lst2.append(node)
+            nodes2.append(node)
 
-    result['degree']["prefix1"]['max'] = network.get_out_degrees(lst1).max()
-    result['degree']["prefix1"]['min'] = network.get_out_degrees(lst1).min()
-    result['degree']["prefix1"]['avg'] = network.get_out_degrees(lst1).mean()
-    result['degree']["prefix1"]["counts"], result['degree']["prefix1"]["bins"] = np.histogram(network.get_out_degrees(lst1), bins=15)  # result['degree']["total"]["bins"].shape[0]
+    result['num_nodes']['prefix1'] = len(nodes1)
+    result['degree']["prefix1"]['max'] = network.get_out_degrees(nodes1).max()
+    result['degree']["prefix1"]['min'] = network.get_out_degrees(nodes1).min()
+    result['degree']["prefix1"]['avg'] = network.get_out_degrees(nodes1).mean()
+    result['degree']["prefix1"]["counts"], result['degree']["prefix1"]["bins"] = np.histogram(network.get_out_degrees(nodes1), bins=15)  # result['degree']["total"]["bins"].shape[0]
+    result['degree']["prefix1"]["d"] = network.get_out_degrees(nodes1)  # result['degree']["total"]["bins"].shape[0]
     if prefix1 == prefix2:
-        lst2 = lst1
-    result['degree']["prefix2"]['max'] = network.get_out_degrees(lst2).max()
-    result['degree']["prefix2"]['min'] = network.get_out_degrees(lst2).min()
-    result['degree']["prefix2"]['avg'] = network.get_out_degrees(lst2).mean()
-    result['degree']["prefix2"]["counts"], result['degree']["prefix2"]["bins"] = np.histogram(network.get_out_degrees(lst2), bins=15)
+        nodes2 = nodes1
+    result['num_nodes']['prefix2'] = len(nodes2)
+    result['degree']["prefix2"]['max'] = network.get_out_degrees(nodes2).max()
+    result['degree']["prefix2"]['min'] = network.get_out_degrees(nodes2).min()
+    result['degree']["prefix2"]['avg'] = network.get_out_degrees(nodes2).mean()
+    result['degree']["prefix2"]["counts"], result['degree']["prefix2"]["bins"] = np.histogram(network.get_out_degrees(nodes2), bins=15)
+    result['degree']["prefix2"]["d"] = network.get_out_degrees(nodes2)  # result['degree']["total"]["bins"].shape[0]
 
     result['weights'] = {}
     weights = []
@@ -138,7 +242,8 @@ def get_descriptors(network, short_name, nx_network, already_calculated=False):
         weight = nx_network.get_edge_data(v1, v2)['weight']
         weights.append(weight)
 
-    result['weights']['counts'], result['weights']['bins'] = np.histogram(weights, bins=8)
+    # result['weights']['counts'], result['weights']['bins'] = np.histogram(weights, bins=8)
+    result['weights']['d'] = weights
 
     # estimated diamater  and longest path
     d, (v1, v2) = top.pseudo_diameter(network)
@@ -175,9 +280,11 @@ def pprint(results):
         "Total Max degree: {}\n"
         "Total Min degree: {}\n"
         "Total Avg degree: {}\n"
+        "Prefix1 num. nodes: {}\n"
         "Prefix1 Max degree: {}\n"
         "Prefix1 Min degree: {}\n"
         "Prefix1 Avg degree: {}\n"
+        "Prefix2 num. nodes: {}\n"
         "Prefix2 Max degree: {}\n"
         "Prefix2 Min degree: {}\n"
         "Prefix2 Avg degree: {}\n"
@@ -187,14 +294,16 @@ def pprint(results):
         "Num Components: {}\n"
     ).format(
         results['name'],
-        results['num_nodes'],
+        results['num_nodes']['total'],
         results['num_edges'],
         results['degree']["total"]['max'],
         results['degree']["total"]['min'],
         results['degree']["total"]['avg'],
+        results['num_nodes']["prefix1"],
         results['degree']["prefix1"]['max'],
         results['degree']["prefix1"]['min'],
         results['degree']["prefix1"]['avg'],
+        results['num_nodes']["prefix2"],
         results['degree']["prefix2"]['max'],
         results['degree']["prefix2"]['min'],
         results['degree']["prefix2"]['avg'],
@@ -222,39 +331,24 @@ if __name__ == '__main__':
 
     # for desc in [at_desc, ta_desc, au_desc, ua_desc, uu_desc]:
     for desc in [at_desc, au_desc, uu_desc]:
-        print pprint(desc)
-        # draw_hist(desc['degree']["total"]['bins'], desc["degree"]["total"]['counts'],
-        #           title=desc[''],
-        #           file_name=desc["filename_dd"])
+        # print pprint(desc)
+        # plot_hist(desc['degree']["prefix1"]['d'], title=desc['title_dd1'], file_name=desc["filename_dd1l"], loglog=True)
+        # plot_hist(desc['degree']["prefix1"]['d'], title=desc['title_dd1_acum'], file_name=desc["filename_dd1_acum"], cumulative=True, loglog=True)
+        # plot_hist(desc['degree']["prefix2"]['d'], title=desc['title_dd2'], file_name=desc["filename_dd2l"], loglog=True)
+        # plot_hist(desc['degree']["prefix2"]['d'], title=desc['title_dd2_acum'], file_name=desc["filename_dd2_acum"], cumulative=True, loglog=True)
 
-        draw_hist(desc['degree']["prefix1"]['bins'], desc["degree"]["prefix1"]['counts'],
-                  title=desc['title_dd1'],
-                  file_name=desc["filename_dd1"])
-
-        draw_hist(desc['degree']["prefix2"]['bins'], desc["degree"]["prefix2"]['counts'],
-                  title=desc['title_dd2'],
-                  file_name=desc["filename_dd2"])
-
-        draw_hist(desc["weights"]['bins'], desc["weights"]['counts'],
-                  title=desc['title_wd'],
-                  file_name=desc["filename_wd"])
-
-        draw_hist(desc["weights"]['bins'], desc["weights"]['counts'],
-                  title=desc['title_wd'],
-                  file_name=desc["filename_wdl"], loglog=True)
-
-        # draw_hist(desc['degree']['bins'], desc["degree"]['counts'],
-        #           title=desc['name'],
-        #           file_name=desc["filename_ddl"],
-        #           loglog=True)
+        # if desc != uu_desc:  # user-user network doesn't have weights
+        #     # plot_hist(desc['weights']['d'], title=desc['title_wd'], file_name=desc["filename_wd"])
+        #     plot_hist(desc['weights']['d'], title=desc['title_wd'], file_name=desc["filename_wdl"], loglog=True)
+        #     plot_hist(desc['weights']['d'], title=desc['title_wd_acum'], file_name=desc["filename_wd_acum"], loglog=True, cumulative=True)
 
         if 'components' in desc.keys():
             draw_hist(desc['components']['bins'], desc['components']['counts'],
                       title=desc['title_cd'],
-                      file_name=desc["filename_cd"])
+                      file_name=desc["filename_cdl"], loglog=True, cumulative=True)
 
         if 'sp' in desc.keys():
-            draw_hist(desc['sp']['bins'], desc['sp']['counts'],
+            draw_hist(desc['sp']['bins'][:-1], desc['sp']['counts'],
                       title=desc['title_sp'],
                       file_name=desc['filename_sp'],
-                      bins=10)
+                      nbins=10, loglog=True, cumulative=True)
