b8409aabc77e31d8704375dc22528379f1de3cff
gbg141 <guille_gbg@hotmail.com>
2017-06-13 09:50:42 +0200

    I didn't save the changes, sorry

--- src/scripts.py
+++ src/scripts.py
@@ -7,6 +7,20 @@ from recommender_system import RecommenderSystem
 
 e = RecommenderSystem.instance()
 
+recommendationLengths = [1,5,20]
+maxReferenceArtists = [1,3,5]
+kneighborhood = [1, 2, None]
+maxSimilarUsers = [1, 5]  #(for k=1, 2, 5) 
+
+for recommendationLength in recommendationLengths:
+    for refArtists in maxReferenceArtists:
+        for k in kneighborhood:
+            for simUsers in maxSimilarUsers:
+                e.recommendation_evaluation(maxReferenceArtists=refArtists, kneighborhood=k, maxSimilarUsers=simUsers, recommendationLength=recommendationLength, relevanceLength=None, relevanceAccum=0.95, numUsers=10000)
+
+
+
+'''
 maxReferenceArtists = [3]
 kneighborhood = [1, 2, 5, None]
 maxSimilarUsers1 = [1, 5]  # (for k=1, 2, 5)
@@ -17,3 +31,4 @@ for refArtists in maxReferenceArtists:
         maxSimilarUsers = maxSimilarUsers1 if k else maxSimilarUsers2
         for simUsers in maxSimilarUsers:
             e.recommendation_evaluation(maxReferenceArtists=refArtists, kneighborhood=k, maxSimilarUsers=simUsers, recommendationLength=10, relevanceLength=None, relevanceAccum=0.95, numUsers=10000)
+'''
