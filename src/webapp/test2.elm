56a712e71011e9c790e93cb5c0192e2e1e131a7f
Carles <cmirallesp@gmail.com>
2017-06-16 18:29:33 +0200

    Backup

--- src/webapp/test2.elm
+++ src/webapp/test2.elm
@@ -1,12 +1,16 @@
 import Json.Decode exposing (field, int, string, Decoder)
-import Html exposing (text, div, input, button, p, select,label, Html)
-import Html.Attributes exposing (value,multiple)
+import Html exposing (text, div, input, button, p, select,label,h1,h2,h3,h4,h5,h6,fieldset, Html)
+import Html.Attributes exposing (value,multiple,style)
 import Html.Events exposing (onClick, onInput)
 import Html exposing (program)
 import Http exposing (get, Error, Response, Error(..))
 import Task  exposing (Task, succeed, fail, andThen, mapError)   
 
-
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row 
+import Bootstrap.Button as Button
 main =
   Html.program
     { init = init 
@@ -18,7 +22,6 @@ main =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
-
 
 
 -- MODEL
@@ -33,9 +36,15 @@ type alias Model =
   , added: List Artist
   }
 
+model : Model
+model =
+  { artists=[] 
+  , added=[]
+  }
+
 init : (Model, Cmd Msg)
 init =
-  ( Model [] [], Cmd.none )
+  ( model, Cmd.none )
 
 decoder : Decoder (List Artist)
 decoder =
@@ -76,6 +85,8 @@ update msg model =
     LoadArtists (Err _) ->
       (model, Cmd.none)
 
+     -- Boilerplate: Mdl action handler.
+    
 
 getAllArtists : String -> Cmd Msg
 getAllArtists _=
@@ -88,23 +99,53 @@ sendGet msg url decoder =
 
 
 -- VIEW
+viewAllArtists: Model -> List (Html Msg)
+viewAllArtists model=
+  [ div []
+    [ h6 [] [text "Artists"]
+    , Grid.container []
+              (List.map (\artist -> 
+                  Grid.row []
+                    [ Grid.col [] 
+                        [ label [] [ text artist.full_name ]]
+                    , Grid.col []
+                        [ Button.button 
+                          [ Button.roleLink
+                          , Button.onClick (AddArtist artist)
+                          ] [text "Add"]
+                        ]
+                    ]
+              ) model.artists)
+    ]
+  ]
+
+viewSelected: List Artist -> List (Html Msg) 
+viewSelected artists=
+  [ div []
+    [ h6 [] [text "Selected"]
+    , Grid.container []
+              ( List.map (\artist ->
+                  Grid.row []
+                    [ Grid.col []
+                      [ label [] [text artist.full_name]]
+                    ]
+              ) artists)
+    ]
+  ]
 
 view : Model -> Html Msg
 view model =
-        div []
-        [ button [ onClick GetAllArtists ] [ text "Search" ]
-        , div
-            []
-            (List.map (\artist -> 
-              div []
-                [ label [] [ text artist.full_name ]
-                , button [onClick (AddArtist artist)] [text "Add"]
-                ]
-            ) model.artists)
-        , div
-          []
-          (List.map (\artists ->
-            div []
-              [ label [] [text artists.full_name]]) model.added)
-        ]
-        
+  Grid.container []
+  [ CDN.stylesheet -- creates an inline style node with the Bootstrap CSS
+  , Grid.row []
+      [ Grid.col [Col.xs12]
+        [ Button.button [Button.primary, Button.onClick GetAllArtists ] [ text "New User" ] ]
+      ]
+  , Grid.row []
+      [ Grid.col [Col.xs6 ]
+        (viewAllArtists model)  
+
+      , Grid.col [ Col.xs6]
+        (viewSelected model.added)
+      ]
+  ]
